d6df08834c8047dfefe6d85bf482dc93806cbb5e
159357159357asdfghjkl <137482040+159357159357asdfghjkl@users.noreply.github.com>
2024-06-02 21:59:38 +0800

    all bug fixed
    put it in mods/scripts/
    i will update more recently

---
+++ camNotes.hx
@@ -0,0 +1,27 @@
+var camNotes:FlxCamera;
+function onCreatePost(){
+    camNotes = new FlxCamera();
+    camNotes.height = 1300;
+    camNotes.bgColor = 0x00000000;
+    
+    FlxG.cameras.remove(game.camHUD, false);
+    FlxG.cameras.remove(game.camOther, false);
+
+    FlxG.cameras.add(camNotes, false);
+    FlxG.cameras.add(game.camHUD, false);
+    FlxG.cameras.add(game.camOther, false);
+    game.notes.cameras = [camNotes];
+    game.strumLineNotes.cameras = [camNotes];
+    camNotes.angle = game.camHUD.angle;
+    camNotes.zoom = game.camHUD.zoom;
+    camNotes.x = game.camHUD.x;
+    camNotes.y = game.camHUD.y;
+}
+function onUpdate(elapsed){
+    if(game.camZooming)
+        camNotes.zoom = game.camHUD.zoom;
+}
+
+createGlobalCallback('setCamNotes',function(x:Float,y:Float,a:Float){
+camNotes.x=x; camNotes.y=y; camNotes.angle=a;
+});
